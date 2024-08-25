2ce4bbd38021538a33323e7e6a97170f9228ad73
KeviaWang <104704083+KeviaWang@users.noreply.github.com>
2024-08-25 20:50:12 +0800

    Add files via upload

--- demo1/viewapment.h
+++ demo1/viewapment.h
@@ -1,7 +1,17 @@
 #ifndef VIEWAPMENT_H
 #define VIEWAPMENT_H
-
-#include <QWidget>
+#include <QDialog>
+#include<QWidget>
+#include<QUdpSocket>
+#include<QMessageBox>
+#include <QHostInfo>
+#include<QHostAddress>
+#include<QDataStream>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
+#include "login.h"
 
 namespace Ui {
 class viewApMent;
@@ -15,7 +25,7 @@ public:
     explicit viewApMent(QWidget *parent = nullptr);
     ~viewApMent();
 public:
-    void appendOneRow(QString name,QString gender,QString birth,QString height,QString weight,QString disease,QString family_history);
+    void appendOneRow(int row,QString name,QString gender,QString birth,QString height,QString weight,QString disease,QString family_history);
 
 private slots:
     void on_pushButton_2_clicked();
@@ -23,9 +33,15 @@ private slots:
     void on_goutong_clicked();
 
     void on_chufangyizhu_clicked();
+    void read_data();
 
 private:
     Ui::viewApMent *ui;
+    QUdpSocket* m_socket;
+    QHostAddress sql_ip;
+    int sql_port;
+    QHostAddress my_ip;
+    int my_port;
 };
 
 #endif // VIEWAPMENT_H
